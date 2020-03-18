bff3a016ea06010b2d5a01568d96c7df6450e4a9
alwayswannafly <15007159069@163.com>
2020-03-18 21:20:22 +0800

    el-form源码解析

--- element-ui-test2/src/App.vue
+++ element-ui-test2/src/App.vue
@@ -1,13 +1,21 @@
 <template>
     <div id="app">
         <el-form
-            :inline="true"
+            style="width:500px"
+            :inline="false"
             :model="data"
             :rules="rules"
             ref="form"
             :validate-on-rule-change="false"
             status-icon
+            label-width="100px"
+            label-position="top"
+            label-suffix=":~~"
+            :disabled="false"
+            size="small"
+            :hide-required-asterisk="true"
         >
+            <!-- hide-required-asterisk 控制✳号是否显示 -->
             <!-- validate-on-rule-change 为true时 ，在 rules 属性改变后立即触发一次验证 -->
             <el-form-item
                 label="审批人"
@@ -18,7 +26,11 @@
                 <el-input v-model="data.user" placeholder="审批人"></el-input>
             </el-form-item>
             <el-form-item label="活动区域">
-                <el-select v-model="data.region" placeholder="活动区域">
+                <el-select
+                    v-model="data.region"
+                    placeholder="活动区域"
+                    style="width:100%"
+                >
                     <el-option label="区域一" value="shanghai"></el-option>
                     <el-option label="区域二" value="beijing"></el-option>
                 </el-select>
@@ -28,13 +40,13 @@
                 <el-button type="primary" @click="addRule"
                     >添加校验规则</el-button
                 >
-                <el-button type="success" @click="showSuccess"
+                <!-- <el-button type="success" @click="showSuccess"
                     >成功校验</el-button
                 >
                 <el-button type="danger" @click="showError">失败校验</el-button>
                 <el-button type="warning" @click="showValidating"
                     >校验中</el-button
-                >
+                > -->
             </el-form-item>
         </el-form>
     </div>
@@ -113,7 +125,7 @@ export default {
         showValidating() {
             this.status = "validating";
             this.error = "";
-        },
+        }
     }
 };
 </script>

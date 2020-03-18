2a7af742d251ab352ada00dba4aa4de755ef7b53
alwayswannafly <15007159069@163.com>
2020-03-18 14:25:02 +0800

    表单校验的基本用法，/* eslint-disable */ 防止lint检测

--- element-ui-test2/src/App.vue
+++ element-ui-test2/src/App.vue
@@ -1,36 +1,68 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="app">
+        <el-form :inline="true" :model="data" :rules="rules" ref="form">
+            <el-form-item label="审批人" prop="user">
+                <el-input v-model="data.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域">
+                <el-select v-model="data.region" placeholder="活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+        </el-form>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    data() {
+        var userValidator = (rule, value, callback) => {
+            if (value.length > 3) {
+                callback();
+            } else {
+                callback(new Error("用户名长度必须大于3 "));
+            }
+        };
+        return {
+            data: {
+                user: "",
+                region: ""
+            },
+            rules: {
+                user: [
+                    {
+                        required: true,
+                        message: "请输入用户名",
+                        trigger: "blur"
+                    },
+                    { validator: userValidator, trigger: "blur" }
+                ]
+            }
+        };
+    },
+    methods: {
+        /* eslint-disable */
+        onSubmit() {
+            console.log(this.data);
+            this.$refs.form.validate((isValid, err) => {
+                console.log(isValid);
+                // console.log(err);
+
+                if (isValid) {
+                    alert("submit!");
+                } else {
+                    console.log("error submit!!");
+                    return false;
+                }
+            });
+        }
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>

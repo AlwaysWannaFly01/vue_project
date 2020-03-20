64f4a605d8f933abde9c92249ba695c32a3b6211
alwayswannafly <15007159069@163.com>
2020-03-20 20:28:46 +0800

    express基础概念

--- admin-node/app.js
+++ admin-node/app.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const app = express()
 
+// function myLogger(req, res, next) {
+//     console.log('myLogger');
+//     /* 使用中间件，next方法一定要调用，否则不会继续向下执行 */
+//     /* 中间件需要在响应结束前被调用 */
+//     next()
+// }
+// app.use(myLogger)
+
 app.get('/', (req, res) => {
+    throw new Error('error')
     res.send('hello node')
 })
+
+const errorHandler = (err, req, res, next) => {
+    /* 四个参数一个都不能少 */
+    /* 异常处理需要后置 */
+    console.log('errorHandler');
+    res.status(400).json({
+        error: -1,
+        msg: err.toString()
+    })
+    res.send('down...')
+}
+app.use(errorHandler)
+
+// app.post('/user', (req, res) => {
+//     res.send('hello node2')
+// })
+
 const server = app.listen(1224, () => {
     const { address, port } = server.address()
     console.log('HTTP启动成功-http://%s:%s', address, port);
-
 })

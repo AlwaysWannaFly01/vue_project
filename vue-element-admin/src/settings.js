d75deea31e5e939b808c2e81c27bc983f5591bc5
alwayswannafly <15007159069@163.com>
2020-03-20 19:32:41 +0800

    source-map；标题

--- vue-element-admin/src/settings.js
+++ vue-element-admin/src/settings.js
@@ -1,28 +1,32 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: '小慕读书',
 
   /**
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  /* 控制设置按钮显示 */
+  showSettings: false,
 
   /**
    * @type {boolean} true | false
    * @description Whether need tagsView
    */
+  /* 顶部tab标签 */
   tagsView: true,
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
+  /* 头部置顶 */
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
+  /* 左侧Logo */
   sidebarLogo: false,
 
   /**

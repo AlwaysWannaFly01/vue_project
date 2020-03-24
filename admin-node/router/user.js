71695eb02447742f239ef2c925d2dabed0ee1e4e
alwayswannafly <15007159069@163.com>
2020-03-24 22:40:29 +0800

    响应结果封装

--- admin-node/router/user.js
+++ admin-node/router/user.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const Result = require('../models/Result')
 const router = express.Router()
 
-router.post('/login', (req, res) => {
-    console.log(req.body); 
-    
-    res.json({
-        code: 0,
-        msg: '登录成功'
-    })
+router.post('/login', function (req, res) {
+    // console.log(req.body);
+    // console.log(res, 'res');
+    const { username, password } = req.body
+    console.log(username);
+    console.log(password);
+    if (username === 'admin' && password === '111111') {
+        new Result('登录成功').success(res)
+    }else{
+        new Result('登录失败').fail(res)
+    }
 })
 router.get('/info', (req, res, err) => {
     res.json('userinfo...')

558a76424d9636b769e06bbed56964755e22f58a
alwayswannafly <15007159069@163.com>
2020-03-26 17:14:56 +0800

    md5密码加密登录

--- admin-node/router/user.js
+++ admin-node/router/user.js
@@ -6,33 +6,16 @@ const { PWD_SALT } = require('../utlis/constant')
 const router = express.Router()
 
 router.post('/login', function (req, res) {
-    // console.log(req.body);
-    // console.log(res, 'res');
     let { username, password } = req.body
-    // password = md5(`${password}${PWD_SALT}`)
-    // login(username, password).then(user => {
-    //     console.log(user,'user');
-    //     if (!user || user.length === 0) {
-    //         new Result('登录失败').fail(res)
-    //     } else {
-    //         console.log(444);
-            
-    //         new Result('登录成功').success(res)
-    //     }
-    // })
+    password = md5(`${password}${PWD_SALT}`)
+
     login(username, password).then(user => {
-        console.log(user);
         if (!user || user.length === 0) {
-          new Result('登录失败').fail(res)
+            new Result('登录失败').fail(res)
         } else {
-          new Result('登录成功').success(res)
+            new Result('登录成功').success(res)
         }
-      })
-    // if (username === 'admin' && password === '111111') {
-    //     new Result('登录成功').success(res)
-    // } else {
-    //     new Result('登录失败').fail(res)
-    // }
+    })
 })
 router.get('/info', (req, res, err) => {
     res.json('userinfo...')
--- admin-node/utlis/constant.js
+++ admin-node/utlis/constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CODE_ERROR: 1,
+    CODE_ERROR: -1,
     CODE_SUCCESS: 0,
     debug: true,
     PWD_SALT: 'admin_imooc_node'

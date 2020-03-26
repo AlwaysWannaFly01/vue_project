c21344b6001cfde965753b8fe1c04dfb4fc268da
alwayswannafly <15007159069@163.com>
2020-03-26 18:01:52 +0800

    生成JWT_功能开发1

--- admin-node/router/user.js
+++ admin-node/router/user.js
@@ -5,13 +5,15 @@ const { md5 } = require('../utlis')
 const { PWD_SALT } = require('../utlis/constant')
 const { body, validationResult } = require('express-validator')
 const boom = require('boom')
+const jwt = require('jsonwebtoken')
+const { PRIVATE_KEY, JWT_EXPIRED } = require('../utlis/constant')
 const router = express.Router()
 
 router.post(
     '/login',
     [
         body('username').isString().withMessage('用户名必须为字符'),
-        body('password').isNumeric().withMessage('密码必须为数字')
+        body('password').isString().withMessage('密码必须为字符')
     ],
     function (req, res, next) {
         const err = validationResult(req)
@@ -26,7 +28,14 @@ router.post(
                 if (!user || user.length === 0) {
                     new Result('登录失败').fail(res)
                 } else {
-                    new Result('登录成功').success(res)
+                    console.log(user, 'user');
+                    // const [_user] = user
+                    const token = jwt.sign(
+                        { username },
+                        PRIVATE_KEY,
+                        { expiresIn: JWT_EXPIRED }
+                    )
+                    new Result({ token }, '登录成功').success(res)
                 }
             })
         }

cde8094a33507ffdf558240e53d62e8dc4ee54f4
AlwaysWannaFly <15007159069@163.com>
2020-07-14 22:46:26 +0800

    多页面在新窗口跳转

--- multipage-demo/src/js/One.vue
+++ multipage-demo/src/js/One.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="One">
     One
+    <button @click="click">多页面跳转到Two页面</button>
+    <!-- <a href= "two.html">链接跳转到Two页面</a> -->
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'One'
-}
+  name: "One",
+  methods: {
+    click() {
+      // this.$router.push({path: "/two", query: {id: 12}})
+      // const new2 = this.$router.resolve({ name: "/two", params: { id: 1 } });
+      // console.log(new2);
+      // window.open(new2.location.name, "_blank");
+
+      //在新窗口打开
+      const { href } = this.$router.resolve({
+        path: "/two",
+        query: {
+          id:22
+        }
+      });
+      window.open(href, "_blank");
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
--- multipage-demo/src/router/index.js
+++ multipage-demo/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import One from '../js/One.vue'
+import Two from '../js/Two.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/one',
+      name: 'One',
+      component: One
+    },
+    {
+      path: '/two',
+      name: 'Two',
+      component: Two
     }
   ]
 })
